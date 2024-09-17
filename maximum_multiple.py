ffe80cd03d0f2b6e6c21d2174b40ec8512bb194c
EarthSub <illisky.dim@gmail.com>
2024-09-18 00:15:08 +0300

    del

--- maximum_multiple.py
+++
@@ -1,8 +0,0 @@
-divisor = int(input())
-boundary = int(input())
-
-if divisor > 0:
-    for divisible in range(boundary, divisor-1, -1):
-        if divisible % divisor == 0:
-            break
-print(divisible)
