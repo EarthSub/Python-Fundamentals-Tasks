8ab89dc3f5e150b89f0a30f8cc42c69fa37c2226
EarthSub <illisky.dim@gmail.com>
2024-09-17 23:57:50 +0300

    sys

---
+++ drink_something.py
@@ -0,0 +1,12 @@
+person_age = int(input())
+drink = ""
+
+if person_age <= 14:
+    drink = "toddy"
+elif person_age <= 18:
+    drink = "coke"
+elif person_age <= 21:
+    drink = "beer"
+else:
+    drink = "whisky"
+print(f"drink {drink}")
