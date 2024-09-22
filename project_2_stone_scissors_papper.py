06ac0be7c1b86142077ccb8df336ffbcb8fbfdb0
EarthSub <illisky.dim@gmail.com>
2024-09-22 22:00:22 +0300

    Project-2 Rock-Scissors-Paper

---
+++ project_2_stone_scissors_papper.py
@@ -0,0 +1,35 @@
+import random
+
+rock = "rock"
+scissors = "scissors"
+paper = "paper"
+
+players_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+
+if players_move == "r":
+    players_move = rock
+elif players_move == "s":
+    players_move = scissors
+elif players_move == "p":
+    players_move = paper
+else:
+    raise SystemExit("Invalid Input. Try again...")
+
+computer_random_number = random.randint(1, 3)
+computer_move = ""
+
+if computer_random_number == 1:
+    computer_move = rock
+elif computer_random_number == 2:
+    computer_move = scissors
+elif computer_random_number == 3:
+    computer_move = paper
+
+if (players_move == rock and computer_move == scissors) or \
+        (players_move == paper and computer_move == rock) or \
+        (players_move == scissors and computer_move == paper):
+    print("You win!")
+elif players_move == computer_move:
+    print("Draw!")
+else:
+    print("You lose!")
